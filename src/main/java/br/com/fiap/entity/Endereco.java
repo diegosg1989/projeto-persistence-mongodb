3477632c90d103c3967085fd288a66a8e03924cb
diegosg1989 <diego.sguerra@gmail.com>
2020-08-23 23:56:40 -0300

    ultimas alterações

--- src/main/java/br/com/fiap/entity/Endereco.java
+++ src/main/java/br/com/fiap/entity/Endereco.java
@@ -1,6 +1,5 @@
 package br.com.fiap.entity;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
